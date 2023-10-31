fd5633d427a9db385a4dafc536ce739348ab31de
nishantak09 <nishantak09@gmail.com>
2023-10-31 20:24:54 +0530

    greatness

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import joinedload
 from datetime import datetime
 from markupsafe import escape
 import json
@@ -8,25 +9,53 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql:///test.db'
 db = SQLAlchemy(app)
 
-class Todo(db.Model):
+
+class User(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
-	content = db.Column(db.String(200), nullable=False)
-	completed = db.Column(db.Integer, default=0)
-	date_created = db.Column(db.DateTime, default=datetime.utcnow)
+	username = db.Column(db.String(80), unique=True, nullable=False)
+	email = db.Column(db.String(120), unique=True, nullable=False)
 
 	def __repr__(self):
 		return '<Task %r>' % self.id
 
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
+    body = db.Column(db.Text, nullable=False)
+    pub_date = db.Column(db.DateTime, nullable=False,
+        default=datetime.utcnow)
+
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'),
+        nullable=False)
+    category = db.relationship('Category',
+        backref=db.backref('posts', lazy=True))
+
+    def __repr__(self):
+        return '<Post %r>' % self.title
+
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+
+    def __repr__(self):
+        return '<Category %r>' % self.name
+
+
 @app.route('/')
+def reg():
+	return render_template('register.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/home')
 def home():
-	return render_template('index.html')
-
-app.route('/user/<username>')
-def show_user_profile(username):
-    return f'User {escape(username)}'
-
-
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
 	app.run()
+	#db.create_all()
