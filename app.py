a8c02cecdc9a8b72d1a8578b653827d551a5a41e
nishantak09 <nishantak09@gmail.com>
2023-10-31 19:24:40 +0530

    multiple stuff

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 from markupsafe import escape
+import json
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql:///test.db'
+db = SQLAlchemy(app)
 
-# The route() function of the Flask class is a decorator, 
-# which tells the application which URL should call 
-# the associated function.
+class Todo(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	content = db.Column(db.String(200), nullable=False)
+	completed = db.Column(db.Integer, default=0)
+	date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+	def __repr__(self):
+		return '<Task %r>' % self.id
+
 @app.route('/')
 def home():
 	return render_template('index.html')
@@ -14,7 +25,8 @@ app.route('/user/<username>')
 def show_user_profile(username):
     return f'User {escape(username)}'
 
-# main driver function
-if __name__ == '__main__':
 
+
+
+if __name__ == '__main__':
 	app.run()
