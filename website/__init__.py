e34b20ebf337c3d7ada73ef5aa667584f71ae678
nishantak09 <nishantak09@gmail.com>
2023-11-01 18:20:19 +0530

    auth-noauth

--- website/__init__.py
+++ website/__init__.py
@@ -13,17 +13,15 @@ def create_app():
     db.init_app(app)
 
     from .views import views
-    from .auth import auth
 
     app.register_blueprint(views, url_prefix='/')
-    app.register_blueprint(auth, url_prefix='/')
     from .models import User,Blogs 
 
     with app.app_context():
         db.create_all()
 
     login_manager = LoginManager()
-    login_manager.login_view = 'auth.login'
+    login_manager.login_view = 'views.login'
     login_manager.init_app(app)
 
     @login_manager.user_loader
--- website/auth.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template, url_for, request, flash
-
-auth = Blueprint('auth', __name__)
-
-@auth.route('/login', methods=['GET', 'POST'])
-def login():
-    return render_template('login.html')
